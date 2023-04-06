7e9cb32736a60a092705a2346ec07c33f02ef692
Abeer-Yahya <abeer.yahya799@gmail.com>
2023-04-06 12:38:45 +0300

    Completed Career Cards section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./assets/scss/root.css";
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Numbers from "./components/numbers";
+import Career from "./components/career";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
       <Numbers />
+      <Career />
     </div>
   );
 }
---
+++ src/components/career.jsx
@@ -0,0 +1,207 @@
+import React from "react";
+export default function Career() {
+  return (
+    <section className="career">
+      <h2>Choose a career</h2>
+      <p>
+        Practicum bootcamps are fully online and part-time. You can keep your
+        current job while learning a new profession. It’s the no-risk,
+        high-reward option.
+      </p>
+      <div className="careerCards">
+        <a href="#" className="card">
+          <div style={{ width: "360px" }}>
+            <span>Challenge level</span>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3233-3034-4261-a537-326438353435__group_121199.svg"
+              alt="3.5"
+            />
+            <h3>Software Engineering Bootcamp</h3>
+            <div
+              className="cols"
+              style={{ justifyContent: "flex-start", gap: "4.6rem" }}
+            >
+              <p>10 months</p>
+              <p>$10,000</p>
+            </div>
+
+            <a href="#">Learn more</a>
+          </div>
+          <div style={{ width: "300px" }}>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3031-3566-4737-b730-616532303634__se.svg"
+              alt="a man catching flies"
+              className="middleImg"
+              style={{ marginLeft: "-2rem" }}
+            />
+          </div>
+          <div style={{ width: "360px" }}>
+            <h5>Your future job title</h5>
+            <p>
+              Software Engineer / Web Developer / Full-Stack Developer /
+              Full-Stack Software Engineer
+            </p>
+            <h5>Skillset</h5>
+            <p>
+              Full-Stack: JavaScript, React.js, HTML, CSS, Babel, Webpack, NPM,
+              GitHub, Git, Rest API, Google Cloud, MongoDB, Node.js
+            </p>
+            <h5>Median graduate starting salary: $75,100</h5>
+          </div>
+        </a>
+        <a href="#" className="card">
+          <div style={{ width: "360px" }}>
+            <span>Challenge level</span>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3630-6238-4662-b235-626135663562__group_121198.svg"
+              alt="4"
+            />
+            <h3>Data Science Bootcamp</h3>
+            <div
+              className="cols"
+              style={{ justifyContent: "flex-start", gap: "4.6rem" }}
+            >
+              <p>9 months</p>
+              <p>$12,500</p>
+            </div>
+
+            <a href="#">Learn more</a>
+          </div>
+          <div style={{ width: "300px" }}>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3530-6135-4865-b735-643533633565__ds.svg"
+              alt="a man catching flies"
+              className="middleImg"
+              style={{ marginLeft: "-2rem" }}
+            />
+          </div>
+          <div style={{ width: "360px" }}>
+            <h5>Your future job title</h5>
+            <p>
+              Data Scientist / Data Science Researcher/ Machine Learning
+              Engineer / ML Scientist
+            </p>
+            <h5>Skillset</h5>
+            <p>
+              Python, NLTK, Numpy, Plotly, Dash, Seaborn, Tableau, Matplotlib,
+              Scikit-learn, Pandas, Jupyter Notebook, LightGBM, PySpark, Keras
+            </p>
+            <h5>Median graduate starting salary: $89,300</h5>
+          </div>
+        </a>
+        <a href="#" className="card">
+          <div style={{ width: "360px" }}>
+            <span>Challenge level</span>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3930-3531-4738-b137-353136333933__group_121196.svg"
+              alt="3"
+            />
+            <h3>Data Analytics Bootcamp</h3>
+            <div
+              className="cols"
+              style={{ justifyContent: "flex-start", gap: "4.6rem" }}
+            >
+              <p>7 months</p>
+              <p>$8,000</p>
+            </div>
+
+            <a href="#">Learn more</a>
+          </div>
+          <div style={{ width: "300px" }}>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3538-3536-4461-b432-383833386438__da.svg"
+              alt="a man catching flies"
+              style={{ marginLeft: "-2rem" }}
+            />
+          </div>
+          <div style={{ width: "360px" }}>
+            <h5>Your future job title</h5>
+            <p>
+              Data Analyst / Product analyst/ Business Analyst / Growth Analyst
+              / Data and Insights Analyst
+            </p>
+            <h5>Skillset</h5>
+            <p>
+              Google Sheets, SQL, Python, Tableau, Pandas, Scikit-learn,
+              Matplotlib
+            </p>
+            <h5>Median graduate starting salary: $75,100</h5>
+          </div>
+        </a>
+        <a href="#" className="card">
+          <div style={{ width: "360px" }}>
+            <span>Challenge level</span>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3364-3531-4239-a162-636339376533__group_121195_2.svg"
+              alt="1"
+            />
+            <h3>Quality Assurance Bootcamp</h3>
+            <div
+              className="cols"
+              style={{ justifyContent: "flex-start", gap: "4.6rem" }}
+            >
+              <p>5 months</p>
+              <p>$4,200</p>
+            </div>
+
+            <a href="#">Learn more</a>
+          </div>
+          <div style={{ width: "300px" }}>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3865-3036-4332-a334-343032623866__qa.svg"
+              alt="a man catching flies"
+              className="middleImg"
+            />
+          </div>
+          <div style={{ width: "360px" }}>
+            <h5>Your future job title</h5>
+            <p>Quality Assurance Engineer </p>
+            <h5>Skillset</h5>
+            <p>Postman, Charles, Console, JavaScript, SQL, Jira, Playright</p>
+            <h5>Median graduate starting salary: $67,000</h5>
+          </div>
+        </a>
+        <a href="#" className="card">
+          <div style={{ width: "360px" }}>
+            <span>Challenge level</span>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3930-3531-4738-b137-353136333933__group_121196.svg"
+              alt="3"
+            />
+            <h3>Business Intelligence Analytics Bootcamp</h3>
+            <div
+              className="cols"
+              style={{ justifyContent: "flex-start", gap: "4.6rem" }}
+            >
+              <p>6 months</p>
+              <p>$7,000</p>
+            </div>
+
+            <a href="#">Learn more</a>
+          </div>
+          <div style={{ width: "300px" }}>
+            <img
+              src="https://t.cf.practicum.com/usa-main/tild3664-6561-4366-a330-353932613066__bi.svg"
+              alt="a man catching flies"
+              className="middleImg"
+              style={{ marginLeft: "-2rem" }}
+            />
+          </div>
+          <div style={{ width: "360px" }}>
+            <h5>Your future job title</h5>
+            <p>
+              Software Engineer / Web Developer / Full-Stack Developer /
+              Full-Stack Software Engineer
+            </p>
+            <h5>Skillset</h5>
+            <p>
+              Full-Stack: JavaScript, React.js, HTML, CSS, Babel, Webpack, NPM,
+              GitHub, Git, Rest API, Google Cloud, MongoDB, Node.js
+            </p>
+            <h5>Median graduate starting salary: $75,100</h5>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+}
